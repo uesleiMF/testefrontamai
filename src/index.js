b9edaaebc608cb612b31f15ad3125ccb6f2d91a6
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-04-13 22:01:24 -0300

    k

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Switch, Redirect, Route } from 'react-router';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
+import { BrowserRouter} from 'react-router-dom';
 
 import Login from './Login';
 import Register from './Register';
 import Dashboard from './Dashboard';
+import Home from "./pages/Home/Home";
+import Sobrenos from "./pages/Sobrenos/Sobrenos";
+
+
 import './Login.css';
 
 ReactDOM.render(
@@ -15,6 +19,8 @@ ReactDOM.render(
             <Route exact path='/register' component={Register} />
             <Route path='/dashboard' component={Dashboard} />
            
+            <Route path= '/home' component={Home} />
+            <Route path= '/sobrenos' component={Sobrenos} />
             {/* <Route component={NotFound}/> */}
         </Switch>
     </BrowserRouter>,
