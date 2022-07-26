ccf418f3f3093a5994750e56b6ed598789900a65
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-07-26 18:53:11 -0300

    n b

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -50,7 +50,7 @@ export default class Dashboard extends Component {
     if (this.state.search) {
       data = `${data}&search=${this.state.search}`;
     }
-    axios.get(`https://projeto----amai.herokuapp.com/get-product${data}`, {
+    axios.get(`http://localhost:2000/get-product${data}`, {
       headers: {
         'token': this.state.token
       }
@@ -67,7 +67,7 @@ export default class Dashboard extends Component {
   }
 
   deleteProduct = (id) => {
-    axios.post('https://projeto----amai.herokuapp.com/delete-product', {
+    axios.post('http://localhost:2000/delete-product', {
       id: id
     }, {
       headers: {
@@ -110,7 +110,7 @@ export default class Dashboard extends Component {
       this.setState({ fileName: e.target.files[0].name }, () => { });
     }
     this.setState({ [e.target.name]: e.target.value }, () => { });
-    if (e.target.name === 'search') {
+    if (e.target.name == 'search') {
       this.setState({ page: 1 }, () => {
         this.getProduct();
       });
@@ -126,7 +126,7 @@ export default class Dashboard extends Component {
     file.append('discount', this.state.discount);
     file.append('price', this.state.price);
 
-    axios.post('https://projeto----amai.herokuapp.com/add-product', file, {
+    axios.post('http://localhost:2000/add-product', file, {
       headers: {
         'content-type': 'multipart/form-data',
         'token': this.state.token
@@ -164,7 +164,7 @@ export default class Dashboard extends Component {
     file.append('discount', this.state.discount);
     file.append('price', this.state.price);
 
-    axios.post('https://projeto----amai.herokuapp.com/update-product', file, {
+    axios.post('http://localhost:2000/update-product', file, {
       headers: {
         'content-type': 'multipart/form-data',
         'token': this.state.token
@@ -205,7 +205,7 @@ export default class Dashboard extends Component {
   };
 
   handleProductClose = () => {
-    this.setState({ openProductModal: true});
+    this.setState({ openProductModal: false });
   };
 
   handleProductEditOpen = (data) => {
@@ -221,7 +221,7 @@ export default class Dashboard extends Component {
   };
 
   handleProductEditClose = () => {
-    this.setState({ openProductEditModal: false});
+    this.setState({ openProductEditModal: false });
   };
 
   render() {
@@ -322,7 +322,7 @@ export default class Dashboard extends Component {
               Cancel
             </Button>
             <Button
-              disabled={this.state.name === '' || this.state.desc === '' || this.state.discount === '' || this.state.price === ''}
+              disabled={this.state.name == '' || this.state.desc == '' || this.state.discount == '' || this.state.price == ''}
               onClick={(e) => this.updateProduct()} color="primary" autoFocus>
               Edit Product
             </Button>
@@ -386,6 +386,9 @@ export default class Dashboard extends Component {
                 id="standard-basic"
                 type="file"
                 accept="image/*"
+                // inputProps={{
+                //   accept: "image/*"
+                // }}
                 name="file"
                 value={this.state.file}
                 onChange={this.onChange}
@@ -403,7 +406,7 @@ export default class Dashboard extends Component {
               Cancel
             </Button>
             <Button
-              disabled={this.state.name === '' || this.state.desc === '' || this.state.discount === '' || this.state.price === '' || this.state.file=== null}
+              disabled={this.state.name == '' || this.state.desc == '' || this.state.discount == '' || this.state.price == '' || this.state.file == null}
               onClick={(e) => this.addProduct()} color="primary" autoFocus>
               Add Product
             </Button>
@@ -440,7 +443,7 @@ export default class Dashboard extends Component {
                   <TableCell align="center" component="th" scope="row">
                     {row.name}
                   </TableCell>
-                  <TableCell align="center"><img src={`https://projeto----amai.herokuapp.com/${row.image}`} width="70" height="70" /></TableCell>
+                  <TableCell align="center"><img src={`http://localhost:2000/${row.image}`} width="70" height="70" /></TableCell>
                   <TableCell align="center">{row.desc}</TableCell>
                   <TableCell align="center">{row.price}</TableCell>
                   <TableCell align="center">{row.discount}</TableCell>
