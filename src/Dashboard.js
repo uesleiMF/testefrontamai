7f85e24c8bcac547f580493f0d4ed44c91ab7b19
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-08-19 15:32:36 -0300

    b

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -12,7 +12,7 @@ export default class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-      token: '',
+     /* token: '',*/
       openProductModal: false,
       openProductEditModal: false,
       id: '',
