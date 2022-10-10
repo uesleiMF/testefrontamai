d4b2d55b75536e0a46f62e248788b99f68ef627e
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-10-10 19:46:08 -0300

    89

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -43,58 +43,40 @@ export default function Home() {
                          
                 
     <Carousel>
-        <div>
             <img src={c0} alt="bo" height= '300px' />
 
-            <p className="">LegendA 1 </p>
-        </div>
-        <div>
+        
 
         <img src={c1} alt="bo" height= '300px'/>
 
-       <p className="l">Legend 2</p>
-        </div>
-        <div>
+   
        <img src={c2} alt="bo"  height= '300px' />
 
-    <p className="l">Legend 3</p>
-        </div>
-        <div>
+   
         
         <img src={c3} alt="bo" height= '300px' />
 
-    <p className="l">Legend 4</p>
-        </div>
-        <div>
+   
+     
        <img src={c4} alt="bo" height= '300px' />
 
-   <p className="l">Legend 5</p>
-        </div>
-        <div>
+  
        <img src={c5} alt="bo" height= '300px' />
 
-      <p className="l">Legend 6</p>
-        </div>
-        <div>
+    
        <img src={c6} alt="bo" height= '300px' />
 
-  <p className="l">Legend 7</p>
-        </div>
-        <div>
+ 
        <img src={c7} alt="bo" height= '300px' />
 
-  <p className="l">Legend 8</p>
-        </div>
-        <div>
+  
        <img src={c8} alt="bo" height= '300px' />
 
-    <p className="l">Legend 9</p>
-        </div>
-        <div>
+   
        <img src={c9} alt="bo" height= '300px'/>
 
-      <p className="l">Legend 10</p>
-      </div>
+    
+   
       
         
     </Carousel>
