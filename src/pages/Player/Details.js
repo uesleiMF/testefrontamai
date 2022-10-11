931e43187b8ea88f11b74a0b4097d1c1e243eb3b
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-10-10 22:09:23 -0300

    l

--- src/pages/Player/Details.js
+++ src/pages/Player/Details.js
@@ -4,7 +4,7 @@ function Details(props) {
     return (
         <div className="c-player--details">
             <div className="details-img">
-                <img src={props.song.img_src} alt="" width="200px"/>
+                <img src={props.song.img_src} alt="" width="250px" height="100px"/>
             </div>
             <h3 className="details-title">{props.song.title}</h3>
             <h4 className="details-artist">{props.song.artist}</h4>
