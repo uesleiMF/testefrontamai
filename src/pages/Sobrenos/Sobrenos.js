30993788964f56b3b62ee5366e6cdb49a2e6bbbf
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-11-22 20:52:12 -0300

    56

--- src/pages/Sobrenos/Sobrenos.js
+++ src/pages/Sobrenos/Sobrenos.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Social } from "./styles";
 import { FaWhatsapp } from "react-icons/fa";
-import c1 from "../../components/Img/c1.jpg";
+import c1 from "../../components/Img/uj.png";
 import c2 from "../../components/Img/c2.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Sobrenos.css";
@@ -87,66 +87,77 @@ ls&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR
 
         <div className="row mb-9">
           <div class="col-4 p-3">
-            <div className="card">
-              <img
-                src={c1}
-                alt="bo"
-                width="25px"
-                height="100px"
-                className="card-img-top"
-              />
-              <div className="card-body">
-                <h8 className="card-title">FOLHA 17 QUADRA 13 LOTE 09</h8>
-                <span className="badge bg-primary"></span>
-              </div>
-              <Social>
-                <a
-                  href="  https://api.whatsapp.com/send?phone=5591982390708&data=AWDy3Hf-oQfWn7Syzd8pJ_MlOjYIoghvjhfgubtS_qKP9hVbOUKymlrhXKhaxMF_4Z7ypOsbpTadf2EkRTGbXsjO7NYeJx3i
+            <div class="cardd">
+              <img src={c1} alt="bo" className="card-img-top" />
+
+              <div class="card-body">
+                <h9 class="card-title">TITULO</h9>
+                <p class="card-text">text to build on the card.
+                <Social>
+                  <a
+                    href="  https://api.whatsapp.com/send?phone=5591982390708&data=AWDy3Hf-oQfWn7Syzd8pJ_MlOjYIoghvjhfgubtS_qKP9hVbOUKymlrhXKhaxMF_4Z7ypOsbpTadf2EkRTGbXsjO7NYeJx3i
 iOpao7H5F8u49KPACy0Wy4CObKp6eaOXZy0QClfFlEQ-jGMKpU1qPCD-2r6HchNmCDZ5CpGwRa-v7iOnavOjIFO5H7oW-C1wvYOEuYgUuHue3xsLTHXo9-4Aw2N5ywN1XdQ3TH8h53tHzjPlSC1cdFh9x2Ot4lWxWW
 m6gswbmRrrizJGSQPBvKtAw5IX4QByzZhWL4r6wHPt5aufjRUsxeBDqHQ7r6hpw
 ls&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR
 3Uh-FxfqVWMeHC96SiWFGNArvfLqF4i93-9suwF9cKvcduJ2QyAqAlVzs/"
-                >
-                  <FaWhatsapp />
-                </a>
-              </Social>
-            </div>
-          </div>
-
-          <div class="col-4 p-3">
-            <div className="card">
-              <a href="https://maps.app.goo.gl/e9p6hr2WNqNPU19V9">
-                <img
-                  src={c2}
-                  alt="bo"
-                  width="25px"
-                  height="100px"
-                  className="card-img-top"
-                />
-              </a>
-
-              <div className="card-body">
-                <h8 className="card-title">FOLHA 17 QUADRA 13 LOTE 09</h8>
-                <span className="badge bg-primary"></span>
+                  >
+                    <FaWhatsapp />
+                  </a>
+              
+                </Social>
+                </p>
               </div>
             </div>
           </div>
 
           <div class="col-4 p-3">
-            <div className="card">
-              <a href="https://maps.app.goo.gl/e9p6hr2WNqNPU19V9">
-                <img
-                  src={c1}
-                  alt="bo"
-                  width="25px"
-                  height="100px"
-                  className="card-img-top"
-                />
-              </a>
+            <div class="cardd">
+              <x>
+                <a href="https://maps.app.goo.gl/e9p6hr2WNqNPU19V9">
+                  <img src={c1} alt="bo" className="card-img-top" />
+                </a>
+              </x>
+              <div class="card-body">
+                <h9 class="card-title">TITULO</h9>
+                <p class="card-text">text to build on the card.
+                <Social>
+                  <a
+                    href="  https://api.whatsapp.com/send?phone=5591982390708&data=AWDy3Hf-oQfWn7Syzd8pJ_MlOjYIoghvjhfgubtS_qKP9hVbOUKymlrhXKhaxMF_4Z7ypOsbpTadf2EkRTGbXsjO7NYeJx3i
+iOpao7H5F8u49KPACy0Wy4CObKp6eaOXZy0QClfFlEQ-jGMKpU1qPCD-2r6HchNmCDZ5CpGwRa-v7iOnavOjIFO5H7oW-C1wvYOEuYgUuHue3xsLTHXo9-4Aw2N5ywN1XdQ3TH8h53tHzjPlSC1cdFh9x2Ot4lWxWW
+m6gswbmRrrizJGSQPBvKtAw5IX4QByzZhWL4r6wHPt5aufjRUsxeBDqHQ7r6hpw
+ls&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR
+3Uh-FxfqVWMeHC96SiWFGNArvfLqF4i93-9suwF9cKvcduJ2QyAqAlVzs/"
+                  >
+                    <FaWhatsapp />
+                  </a>
+                </Social>
+                </p>
+              </div>
+            </div>
+          </div>
 
-              <div className="card-body">
-                <h8 className="card-title">FOLHA 17 QUADRA 13 LOTE 09</h8>
-                <span className="badge bg-primary"></span>
+          <div class="col-4 p-3">
+            <div class="cardd">
+              <x>
+                <a href="https://maps.app.goo.gl/e9p6hr2WNqNPU19V9">
+                  <img src={c1} alt="bo" className="card-img-top" />
+                </a>
+              </x>
+              <div class="card-body">
+                <h9 class="card-title">TITULO</h9>
+                <p class="card-text">text to build on the card.
+                <Social>
+                  <a
+                    href="  https://api.whatsapp.com/send?phone=5591982390708&data=AWDy3Hf-oQfWn7Syzd8pJ_MlOjYIoghvjhfgubtS_qKP9hVbOUKymlrhXKhaxMF_4Z7ypOsbpTadf2EkRTGbXsjO7NYeJx3i
+iOpao7H5F8u49KPACy0Wy4CObKp6eaOXZy0QClfFlEQ-jGMKpU1qPCD-2r6HchNmCDZ5CpGwRa-v7iOnavOjIFO5H7oW-C1wvYOEuYgUuHue3xsLTHXo9-4Aw2N5ywN1XdQ3TH8h53tHzjPlSC1cdFh9x2Ot4lWxWW
+m6gswbmRrrizJGSQPBvKtAw5IX4QByzZhWL4r6wHPt5aufjRUsxeBDqHQ7r6hpw
+ls&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR
+3Uh-FxfqVWMeHC96SiWFGNArvfLqF4i93-9suwF9cKvcduJ2QyAqAlVzs/"
+                  >
+                    <FaWhatsapp />
+                  </a>
+                </Social>
+                </p>
               </div>
             </div>
           </div>
