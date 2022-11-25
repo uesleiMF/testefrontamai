7298a435f24b9c96b7b337a94e7449c5128395c9
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-11-25 16:06:15 -0300

    159

--- src/pages/Sobrenos/Sobrenos.js
+++ src/pages/Sobrenos/Sobrenos.js
@@ -14,17 +14,18 @@ export default function Sobrenos() {
           <div className="row">
             <div className="col">
               <br></br>
-              <h2 className="mx-3 my-3 text-center text-success">REDE DE CASAIS AMAI SEMPRE</h2>
-              <h4 className="mx-3 my-3 text-center">
-                INFORMAÇÕES SOBRE A LIDERANÇA:</h4>
+              <h2 className="mx-3 my-3 text-center text-success">REDE DE CASAIS</h2>
+              <h3 className="mx-3 my-3 text-center text-success">AMAI SEMPRE</h3>
+              <h4 className="mx-3 my-3 text-center text-success">
+                ****INFORMAÇÕES SOBRE A LIDERANÇA****:</h4>
             </div>
           </div>
         </div>
       </div>
 
       <div className="card mt-4  bg-primary">
-        <h3 className="mx-3 my-3 text-center">REDE AMAI </h3>
-        <h4 className="mx-3 my-3 text-center">CASAIS LIDERES </h4>
+        <h3 className="mx-3 my-3 text-center text-white">REDE AMAI </h3>
+        <h4 className="mx-3 my-3 text-center text-white">CASAIS LIDERES </h4>
 
         <div class="d-flex align-content p-3">
         <div class="d-flex align-content-around flex-wrap">
@@ -35,7 +36,7 @@ export default function Sobrenos() {
               
               <img src={c1} alt="bo" className="card-img-top" />
 
-              <p> <h2 className="card-title text-success">CASAL--LIDER</h2></p>
+              <p> <h2 className="card-title ">CASAL--LIDER</h2></p>
               <h3 className="card-title">Pastores</h3>
               <h4 className="card-title">Marcos</h4>
               <h4 className="card-title"> & </h4>
@@ -60,11 +61,11 @@ ls&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR
 
           <div class="d-flex align-content-around flex-wrap">
             <div class="cardd">
-              <x>
+              
                 <a href="https://maps.app.goo.gl/e9p6hr2WNqNPU19V9">
                   <img src={c1} alt="bo" className="card-img-top" />
                 </a>
-              </x>
+            
               <p> <h2 className="card-title">CASAL--LIDER</h2></p>
               <h3 className="card-title">Pastores</h3>
               <h4 className="card-title">Pina</h4>
@@ -127,11 +128,11 @@ ls&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR
 
           <div class="d-flex align-content-around flex-wrap">
             <div class="cardd">
-              <x>
+             
                 <a href="https://maps.app.goo.gl/e9p6hr2WNqNPU19V9">
                   <img src={c1} alt="bo" className="card-img-top" />
                 </a>
-              </x>
+             
              
               <p> <h2 className="card-title">CASAL</h2></p>
               <h3 className="card-title">VICE-LIDER</h3>
@@ -158,11 +159,11 @@ ls&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR
 
           <div class="d-flex align-content-around flex-wrap">
             <div class="cardd">
-              <x>
+              
                 <a href="https://maps.app.goo.gl/e9p6hr2WNqNPU19V9">
                   <img src={c1} alt="bo" className="card-img-top" />
                 </a>
-              </x>
+              
             
               <p> <h2 className="card-title">CASAL</h2></p>
               <h3 className="card-title">SECRETARIO</h3>
