bfcec7bb57e6e7fcc1f56e17430626d7e12fbe96
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-11-26 23:20:54 -0300

    fa

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,14 @@
 import { Container,List, Logo } from "./styles";
-import {FaHeart,FaRegUser,FaPlayCircle,FaHeartbeat} from "react-icons/fa";
+import {FaRegUser,FaHeartbeat} from "react-icons/fa";
 import {ImManWoman} from "react-icons/im";
-
+import {GiMusicalNotes} from "react-icons/gi";
 import { Link } from 'react-router-dom';
 import React from 'react';
 import c1 from "../../components/Img/ieq2.jpg";
 
 export default function Navbar() {
   
-	
+	GiMusicalNotes
 
   return (
     <Container>
@@ -30,7 +30,7 @@ export default function Navbar() {
       </li>
       
       <li>
-         <FaPlayCircle />
+         <GiMusicalNotes />
          
          <Link to="/app">Hinos Gospel</Link>
             
