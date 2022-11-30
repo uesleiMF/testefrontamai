e92ec52efc35e2d835f56e98ae12cb8e1ee66976
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-11-30 16:21:38 -0300

    audio

--- src/pages/Player/data.js
+++ src/pages/Player/data.js
@@ -1,4 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
+
+
 function chillHop() {
 	return [
 		{
@@ -6,7 +8,7 @@ function chillHop() {
 			cover:
 				"https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
 			artist: "Aso, Middle School, Aviino",
-			audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+			audio: "./Audio/audio.mp3",
 			color: ["#205950", "#2ab3bf"],
 			id: uuidv4(),
 			active: true,
@@ -16,7 +18,7 @@ function chillHop() {
 			cover:
 				"https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
 			artist: "Aiguille",
-			audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+			audio: "./public/music/audio1.mp3",
 			color: ["#EF8EA9", "#ab417f"],
 			id: uuidv4(),
 			active: false,
